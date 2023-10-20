c262bdc7969cc80df69c40541d265f098dda03a2
emilia2001 <emilia.boamba@gmail.com>
2023-10-20 18:25:12 +0300

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Boamba_Emilia_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+
+    }
+
+}
